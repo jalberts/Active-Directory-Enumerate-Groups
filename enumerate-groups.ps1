d3abb363db45cb09d42d6d9c4b85a9ccdaddf228
Joe Alberts <jalberts@gmail.com>
2014-08-11 15:49:53 -0400

    Initial Commit
    Active Directory Group membership by department. This is an example
    using the SPH departments. Subgroups of a department still need to be
    handled. Some errors occurring.

---
+++ enumerate-groups.ps1
@@ -0,0 +1,47 @@
+import-module activedirectory
+
+# Expand this array to include more departments within HBHE SPH.
+# Subgroups of departments are handled by making a multidimensional array entry.
+$departments = @(
+					"ADM", 
+					"BIO", 
+					"EHS",
+					"EPID",
+						# @(
+							# "CSEPH"
+						# ),
+					"HBHE",
+						# @(
+							# "PRC",
+							# "CMHD"
+						# ),
+					"HMP", 
+					"ICS"
+				)
+
+Foreach( $department in $departments ){
+	# Create individual output files for each department
+	$OutFile = [environment]::getfolderpath("mydocuments") + "\enum-groups-$department.txt"
+
+	if (Test-Path $OutFile) {
+		Remove-Item $OutFile
+	}
+	
+	# Get AD Groups under department OU
+	$groups = Get-ADGroup -Properties * -Filter * -SearchBase "ou=$department,ou=Departments,OU=SPH,OU=Organizations,OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu" | sort name
+
+	# Output formatted member list for each AD Group
+	Foreach( $group In $groups )
+	{
+		$group_name_header = $group.Name + "`n-------------"
+		Add-Content -value $group_name_header -Path $OutFile
+		
+		$members = Get-ADGroupMember -Identity $group.Name | sort name | % { $_.Name.split(",")[0] }
+		
+		foreach( $member in $members) {
+			Add-Content -Value $member -Path $OutFile
+		}
+		
+		Add-Content -value "`n" -Path $OutFile
+	}
+}
